9adb74573a3a55aa7a5d57b35964ba213e362a0e
NewmanNya <48142755+NewmanNya@users.noreply.github.com>
2019-07-03 17:18:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 调试代码
+#Raw code for test
 # from  selenium import webdriver
 # chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument('--headless')
@@ -9,6 +9,7 @@
 # print (content)
 # client.quit()
 
+#First code
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -16,22 +17,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
 from bs4 import BeautifulSoup as bs
 import time
+
 
 def init_driver():
     # initiate the driver:
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
-
-
-
+    # driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
+    driver = webdriver.Chrome(chrome_options=chrome_options,executable_path='/usr/bin/twitter/chromedriver')
     # set a default wait time for the browser [5 seconds here]:
     driver.wait = WebDriverWait(driver, 5)
-
+    
     return driver
 
 
@@ -63,19 +62,24 @@ def login_twitter(driver, username, password):
 
     return
 
-def tweet(driver, string, picPath,picName):
+def tweet_pic(driver, string, picPath, picName):
     time.sleep(1)
-
     textInputEle=driver.find_element_by_name('tweet')
     textInputEle.send_keys(string)
-    # picInputEle=driver.find_element_by_name('media_empty')
-    # # picSend=os.path.join(picPath,picName)
-    # picInputEle.send_keys(picPath+picName)
+    picInputEle=driver.find_element_by_name('media_empty')
+    # picSend=os.path.join(picPath,picName)
+    picInputEle.send_keys(picPath+picName)
     time.sleep(5)
     driver.find_element_by_css_selector("button.tweet-action").click()
-
     print("Send tweet")
-
+    
+def tweet_text(driver, string):
+    time.sleep(1)
+    textInputEle=driver.find_element_by_name('tweet')
+    textInputEle.send_keys(string)
+    time.sleep(5)
+    driver.find_element_by_css_selector("button.tweet-action").click()
+    print("Send tweet")
 
 
 if __name__ == "__main__":
@@ -87,6 +91,5 @@ if __name__ == "__main__":
     print("Start login")
     login_twitter(driver, username, password)
     print("Start tweet")
-    tweet(driver, "test",'D:\\',"bg.jpg")
-
+    tweet_text(driver, "test")
     #close_driver(driver)
